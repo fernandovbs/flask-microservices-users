c1bc4918937e684b63e8a183b5382902744a0f24
= <fernandovbsouza@gmail.com>
2017-11-27 06:51:06 -0300

    Database container and class included

--- project/__init__.py
+++ project/__init__.py
@@ -1,13 +1,28 @@
 # project/__init__.py
 
+import os
+import datetime
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 # instantiate the app
 app = Flask(__name__)
 
-#set config
-app.config.from_object('project.config.DevelopmentConfig')
+# set config
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
+db = SQLAlchemy(app)
+
+#Model
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=False, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+	
 @app.route('/ping', methods=['GET'])
 def ping_pong():
 	return jsonify({
